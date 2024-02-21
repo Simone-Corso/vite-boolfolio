ff3e6386c0477f5db0d39c9fe59388d683db16a3
Simone-Corso <scorso26@gmail.com>
2024-02-21 10:50:22 +0100

    ho inserito tutto quello da installare e fatto il resto per i componenti

---
+++ vite-boolfolio/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+
+  import AppMain from './components/AppMain.vue';
+  import AppMain from './components/AppMain.vue';
+
+  export default{ components: { AppHeader, AppMain }
+}
+</script>
+
+<template>
+<AppHeader/>
+<AppMain/>
+
+<h1 class="title text-center">
+  prova di titolo
+</h1>
+</template>
+
+<style lang="scss">
+@use './style/general.scss' as *;
+
+</style>

baef748aa5b2b246e3212f67cd12a2063e23a835
Simone-Corso <scorso26@gmail.com>
2024-02-22 15:23:46 +0100

    ho risolto a mettere vue alla fine del hompagevue

--- vite-boolfolio/src/router.js
+++ vite-boolfolio/src/router.js
@@ -1,1 +1,35 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomePageVue from "./pages/HomePage.vue";
+import ProjectListVue from "./pages/ProjectList.vue";
+import CategoriesListVue from "./pages/CategoriesList.vue";
+
+
+const router = createRouter ({
+    history: createWebHistory(),
+    routes : [
+        {
+            path:'/',
+            name: '',
+            component: HomePageVue
+        },
+        {
+            path:'/Project',
+            name: 'ProjectList',
+            component: ProjectListVue
+        },
+        {
+            path:'/Categories',
+            name: 'Categories',
+            component: CategoriesListVue
+        }
+
+
+
+    ]
+
+
+
+});
+
+
+export {router}

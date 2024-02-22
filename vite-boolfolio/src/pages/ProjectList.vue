e7ef359c6a3ee12ae956891adfc01d573bfbb78e
Simone-Corso <scorso26@gmail.com>
2024-02-22 17:16:31 +0100

    ho cancellato app main perchè era uguale al projectlist

--- vite-boolfolio/src/pages/ProjectList.vue
+++ vite-boolfolio/src/pages/ProjectList.vue
@@ -1,18 +1,64 @@
 <template lang="">
-    <div>
-        <h1>projectList</h1>
-    </div>
-    <AppMain/>
+    <main class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>
+                    I tuoi progetti:
+                </h1>
+                <ul>
+                    <li v-for="project in projects" :key="project.id">
+                        {{ project.title }} -- {{project.type}}
+                        <img :src="project.thumb" alt="" class="image-project">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    
+</main>
 </template>
 <script>
+import axios from 'axios';
 
-import AppMain from '@/components/AppMain.vue';
+   
 
 export default {
     name: 'ProjectList',
-    components: {AppMain},
+    data(){
+       return {
+        projects: [],
+       }
+    },
+
+    methods:{
+        getProjects(){
+            axios.get('http://127.0.0.1:8000/api/project',{
+                params: {
+                }
+            })
+            .then((response)=>{
+                console.log(response);
+                this.projects = response.data.result.data;
+            })
+            .catch(function(error){
+                console.log(error);
+            })
+            .finally(function(){
+
+            });
+        }
+    },
+
+    created(){
+        this.getProjects();
+    }
+
 }
 </script>
-<style lang="">
+<style scoped>
+
+.image-project{
+    height: 100px;
+
+}
     
 </style>
